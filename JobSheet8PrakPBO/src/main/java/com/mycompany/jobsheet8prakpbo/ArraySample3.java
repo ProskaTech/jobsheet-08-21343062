36c9b86d1fa41a73520e23a30d5a187fee6eadcd
21343062 <112645631+21343062@users.noreply.github.com>
2022-10-13 21:21:25 +0700

    Add files via upload
    Nama : Nesa Athallah Ramde Pikuang
    NIM : 21343062

---
+++ JobSheet8PrakPBO/src/main/java/com/mycompany/jobsheet8prakpbo/ArraySample3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jobsheet8prakpbo;
+
+/**
+ * Created by 21343062-Nesa Athallah
+ * @author user
+ */
+public class ArraySample3 {
+    public static void main(String[]args){
+        
+        //String array 4 baris x 2 kolom
+        String[][] dogs = {{"Terry", "Brown"}, //baris ke 0
+                          {"Kristin", "White"},//baris ke 1
+                          {"Toby", "Gray"},//baris ke 2
+                          {"Fido", "Black"}//baris ke 3
+                          };
+        System.out.println(dogs[0][1]);
+        //mengakses variabel dogs dengan indeks baris 0, indeks kolom 0
+    }
+}
---
+++ JobSheet8PrakPBO/src/main/java/com/mycompany/jobsheet8prakpbo/Tugas1HariDalamSeminggu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jobsheet8prakpbo;
+
+/**
+ * Created by 21343062-Nesa Athallah
+ * @author user
+ */
+public class Tugas1HariDalamSeminggu {
+    public static void main(String[]args)
+    {
+        String days[] = {"Monday","Tuesday","Wednesday",
+                        "Thursday","Friday","Saturday","Sunday"};
+        for (int i = 0; i < days.length; i++){
+            System.out.println(days[i]);
+        }
+    }
+}
